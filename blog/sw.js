3aa990b5fc403a3a285de79b30efe0821ce06cd0
peng <1533431376@qq.com>
2022-09-11 12:31:03 +0800

    hexo 20220911

--- blog/sw.js
+++ blog/sw.js
@@ -1,1 +1,104 @@
-const workboxVersion="5.1.3";importScripts("https://storage.googleapis.com/workbox-cdn/releases/5.1.3/workbox-sw.js"),workbox.core.setCacheNameDetails({prefix:"mycpen"}),workbox.core.skipWaiting(),workbox.core.clientsClaim(),workbox.precaching.precacheAndRoute([{revision:"be2c8e609da55a730d1193862673774b",url:"./404.html"},{revision:"b6850026c05f23a816410b9c189f7e0b",url:"./index.html"},{revision:"73e1a92da3a39646678bffb9e7817148",url:"./js/main.js"},{revision:"8dd10dc9edf3b4213252cfa931fcec37",url:"./css/index.css"}],{directoryIndex:null}),workbox.precaching.cleanupOutdatedCaches(),workbox.routing.registerRoute(/\.(?:eot|ttf|woff|woff2)$/,new workbox.strategies.CacheFirst({cacheName:"fonts",plugins:[new workbox.expiration.ExpirationPlugin({maxEntries:1e3,maxAgeSeconds:2592e3}),new workbox.cacheableResponse.CacheableResponsePlugin({statuses:[0,200]})]})),workbox.routing.registerRoute(/^https:\/\/fonts\.googleapis\.com/,new workbox.strategies.StaleWhileRevalidate({cacheName:"google-fonts-stylesheets"})),workbox.routing.registerRoute(/^https:\/\/fonts\.gstatic\.com/,new workbox.strategies.CacheFirst({cacheName:"google-fonts-webfonts",plugins:[new workbox.expiration.ExpirationPlugin({maxEntries:1e3,maxAgeSeconds:2592e3}),new workbox.cacheableResponse.CacheableResponsePlugin({statuses:[0,200]})]})),workbox.googleAnalytics.initialize();
+/*
+ * @Description: sw
+ * @Author: 安知鱼
+ * @Email: 2268025923@qq.com
+ * @Date: 2022-02-22 11:23:58
+ * @LastEditTime: 2022-03-08 12:24:30
+ * @LastEditors: 安知鱼
+ */
+const workboxVersion = '5.1.3';
+
+importScripts(`https://storage.googleapis.com/workbox-cdn/releases/${workboxVersion}/workbox-sw.js`);
+
+workbox.core.setCacheNameDetails({
+  prefix: 'mycpen',
+});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+// 注册成功后要立即缓存的资源列表
+// 具体缓存列表在gulpfile.js中配置，见下文
+workbox.precaching.precacheAndRoute([{"revision":"a862cca4dd53ae7a807e493dd9bb0b74","url":"./404.html"},{"revision":"bbb3c04abaa9ed7786a841f3e50d3924","url":"./index.html"},{"revision":"73e1a92da3a39646678bffb9e7817148","url":"./js/main.js"},{"revision":"8dd10dc9edf3b4213252cfa931fcec37","url":"./css/index.css"}], {
+  directoryIndex: null,
+});
+
+// 清空过期缓存
+workbox.precaching.cleanupOutdatedCaches();
+
+// 图片资源（可选，不需要就注释掉）
+// workbox.routing.registerRoute(
+//   /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+//   new workbox.strategies.CacheFirst({
+//     cacheName: 'images',
+//     plugins: [
+//       new workbox.expiration.ExpirationPlugin({
+//         maxEntries: 1000,
+//         maxAgeSeconds: 60 * 60 * 24 * 30,
+//       }),
+//       new workbox.cacheableResponse.CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//     ],
+//   })
+// )
+
+// 字体文件（可选，不需要就注释掉）
+workbox.routing.registerRoute(
+  /\.(?:eot|ttf|woff|woff2)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'fonts',
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 1000,
+        maxAgeSeconds: 60 * 60 * 24 * 30,
+      }),
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+// 谷歌字体（可选，不需要就注释掉）
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 1000,
+        maxAgeSeconds: 60 * 60 * 24 * 30,
+      }),
+      new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+// jsdelivr的CDN资源（可选，不需要就注释掉）
+// workbox.routing.registerRoute(
+//   /^https:\/\/cdn\.jsdelivr\.net/,
+//   new workbox.strategies.CacheFirst({
+//     cacheName: 'static-libs',
+//     plugins: [
+//       new workbox.expiration.ExpirationPlugin({
+//         maxEntries: 1000,
+//         maxAgeSeconds: 60 * 60 * 24 * 30,
+//       }),
+//       new workbox.cacheableResponse.CacheableResponsePlugin({
+//         statuses: [0, 200],
+//       }),
+//     ],
+//   })
+// )
+
+workbox.googleAnalytics.initialize();

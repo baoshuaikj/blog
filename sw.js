69e794d5bf9eba75e1b91d482900b9d2328e9e2f
peng <1533431376@qq.com>
2022-09-16 23:23:08 +0800

    hexo 20220916

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"e756b14636503db77ee4069bda761fde","url":"./404.html"},{"revision":"892a425303d42e7959d2d345d0006002","url":"./index.html"},{"revision":"73e1a92da3a39646678bffb9e7817148","url":"./js/main.js"},{"revision":"dcf63028465b73430f23b92b224dd980","url":"./css/index.css"}], {
+workbox.precaching.precacheAndRoute([{"revision":"a5b1d65ece557707a7a732f359ad31c6","url":"./404.html"},{"revision":"cdfde4ca7c040447d2c5d0ec03abdc4c","url":"./index.html"},{"revision":"73e1a92da3a39646678bffb9e7817148","url":"./js/main.js"},{"revision":"dcf63028465b73430f23b92b224dd980","url":"./css/index.css"}], {
   directoryIndex: null,
 });
 

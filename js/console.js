fc5e84616b533c1a8f21ae9272512f6741b99ee1
peng <1533431376@qq.com>
2022-10-09 20:44:48 +0800

    hexo 20221009

---
+++ js/console.js
@@ -0,0 +1,21 @@
+if (window.console) {
+    Function.prototype.makeMulti = function () {
+      let l = new String(this);
+      l = l.substring(l.indexOf("/*") + 3, l.lastIndexOf("*/"));
+      return l;
+    };
+    let string = function () {
+      /*
+
+███╗   ███╗██╗   ██╗ ██████╗██████╗ ███████╗███╗   ██╗
+████╗ ████║╚██╗ ██╔╝██╔════╝██╔══██╗██╔════╝████╗  ██║
+██╔████╔██║ ╚████╔╝ ██║     ██████╔╝█████╗  ██╔██╗ ██║
+██║╚██╔╝██║  ╚██╔╝  ██║     ██╔═══╝ ██╔══╝  ██║╚██╗██║
+██║ ╚═╝ ██║   ██║   ╚██████╗██║     ███████╗██║ ╚████║
+╚═╝     ╚═╝   ╚═╝    ╚═════╝╚═╝     ╚══════╝╚═╝  ╚═══╝
+      */
+    };
+    console.log(string.makeMulti());
+    console.log("欢迎访问 %cMycpenの学习笔记", "color:#5ca1ff;font-weight:bold");
+  }
+
